eca8d683c63655cc78b7d94ee1d23c746866a2d8
Egomaniaca <egomaniaca@gmail.com>
2021-12-30 00:19:47 +0300

    starting lesson1

--- script.js
+++ script.js
@@ -5,20 +5,23 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
 
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-];
-
 const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+    return `<div class="goods-item">
+                <h3>${title}</h3>
+                <p>${price}</p>
+            </div>`;
 };
 
 const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList;
-}
+    /*    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join('');
+       console.log(goodsList);
+       document.querySelector('.goods-list').innerHTML = goodsList;
+    */
+    const goodsBlock = document.querySelector('.goods');
+    list.forEach(item => {
+        goodsBlock.innerHTML += renderGoodsItem(item.title, item.price);
+    });
+};
 
-renderGoodsList(goods); првормиащвмиызшиыв
+renderGoodsList(goods);
+
