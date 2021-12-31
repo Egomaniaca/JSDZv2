f17789da1537bd2dc2de9e108afc3b30dc266b9a
Egomaniaca <egomaniaca@gmail.com>
2021-12-31 19:33:34 +0300

    complete dz1

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
+    { id: 1, title: 'Shirt', price: 150 },
+    { id: 2, title: 'Socks', price: 50 },
+    { id: 3, title: 'Jacket', price: 350 },
+    { id: 4, title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-            </div>`;
-};
+const renderGoodsItem = ({ title, price }, img = "https://via.placeholder.com/150x150") => `<div class="goods-item" data-id="${this.id}">
+        <img src="${img}" alt="Photo">
+        <div class="desc">
+            <h3>${title}</h3 >
+            <p>${price}</p>
+            <button class="buy-btn">Купить</button>
+        </div >
+    </div >`;
+
 
 const renderGoodsList = (list) => {
     /*    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join('');
        console.log(goodsList);
        document.querySelector('.goods-list').innerHTML = goodsList;
     */
-    const goodsBlock = document.querySelector('.goods');
+    const goodsBlock = document.querySelector('.goods-list');
+
     list.forEach(item => {
-        goodsBlock.innerHTML += renderGoodsItem(item.title, item.price);
+        goodsBlock.insertAdjacentHTML('beforeend', renderGoodsItem(item));
     });
 };
 
